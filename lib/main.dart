ca61a3e97e346d027a4d2a6902dbd049219b01da
MD Rashedujjaman Reza <68024439+Rashedujjaman@users.noreply.github.com>
2025-06-16 23:34:21 +0600

    Screen routing based on login status configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:srscs/screens/login_screen.dart';
+import 'package:srscs/screens/dashboard_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:srscs/services/firebase_options.dart';
+import 'package:srscs/services/firebase_service.dart';
 import 'package:srscs/services/user_provider.dart';
 
 void main() async {
@@ -29,12 +31,39 @@ void main() async {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  Future<bool> _isLoggedIn(BuildContext context) async {
+    try {
+      String? uid = await FirebaseService().isUserSignedIn();
+
+      if (uid != null && uid.isNotEmpty) {
+        if (context.mounted) {
+          Provider.of<UserProvider>(context, listen: false).setUserId(uid);
+        }
+        return true;
+      }
+      return false;
+    } catch (e) {
+      return false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'SRSCS',
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: FutureBuilder<bool>(
+        future: _isLoggedIn(context),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasData && snapshot.data == true) {
+            return const DashboardScreen();
+          } else {
+            return const LoginScreen();
+          }
+        },
+      ),
     );
   }
 }
